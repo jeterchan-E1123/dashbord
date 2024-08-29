8fadc190dcfada963eb02708819ea3f0b30331ff
stiyqr <88047576+stiyqr@users.noreply.github.com>
2024-08-29 17:23:51 +0800

    Update task_summary.py

--- task_summary.py
+++ task_summary.py
@@ -2,71 +2,18 @@ import pandas as pd
 import boto3
 from dotenv import load_dotenv
 
-from langchain_aws import BedrockLLM, ChatBedrock
+from langchain_aws import BedrockLLM
 from langchain.agents import tool, Tool
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain_experimental.utilities import PythonREPL
 from langchain.tools.render import render_text_description
-from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
-from langchain.output_parsers import PydanticOutputParser
-from langchain_core.pydantic_v1 import BaseModel
+from langchain_core.prompts import PromptTemplate
 
 from dataframes import create_df_tasksum
 
 load_dotenv()
 
 bedrock_client = boto3.client(service_name="bedrock-runtime")
-
-class TaskSummary(BaseModel):
-    """Task Summary Output"""
-    summary: str
-
-def get_chat_prompt():
-    prompt_template = """You are a data analyst for a company, and you have access to a dataframe containing detailed information about employee timesheets. This dataframe includes data such as task names, descriptions, work hours, and other relevant details.
-
-Your role is to respond to questions from the company's managers regarding the data in this timesheet dataframe. When answering their questions, provide only the final result or answer—there's no need to explain the process, calculations, or steps you used to arrive at that result.
-
-Focus on delivering precise, accurate answers directly based on the data, ensuring that your responses are concise and to the point.
-"""
-
-    prompt = ChatPromptTemplate.from_messages(
-        [("system", prompt_template), ("user", "{input}")]
-    )
-
-    return prompt
-
-def get_chat_agent(df):
-    llm = ChatBedrock(model_id='anthropic.claude-v2:1', client=bedrock_client)
-
-    agent = create_pandas_dataframe_agent(
-        llm,
-        df,
-        prompt=get_chat_prompt(),
-        verbose=True,
-        allow_dangerous_code=True,
-        handle_parsing_errors=True
-    )
-
-    return agent
-
-def get_chat_answer(df, question):
-    agent = get_chat_agent(df)
-
-    try:
-        answer = agent.invoke({'input': question})['output']
-    except ValueError as e:
-        answer = str(e)
-        prefix1 = 'An output parsing error occurred. In order to pass this error back to the agent and have it try again, pass `handle_parsing_errors=True` to the AgentExecutor. This is the error:'
-        prefix2 = 'Could not parse LLM output: `'
-        if not answer.startswith(prefix1) and not answer.startswith(prefix2):
-            raise e
-        answer = answer.removeprefix(prefix1).strip()
-        answer = answer.removeprefix(prefix2).removesuffix('`').strip()
-    except:
-        answer = 'Please ask me a simpler question 😔'
-
-    return answer
 
 def get_agent(df):
     tools, rendered_tools = get_tools()
